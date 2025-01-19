e2caa7b5a17ef3b648e67efeda15816dedfb1b38
Daniel Wagner <daniel@wagners.name>
2025-01-19 12:48:19 +0100

    Refactoring

--- main.py
+++ main.py
@@ -1,9 +1,65 @@
-hand = 0
+"""
+
+Schütteln um ein zufälliges Bild auszusuchen
+
+"""
+"""
+
+Knopf A um das Bild zu senden
+
+"""
+"""
+
+Bild empfangen
+
+"""
+"""
+
+Spiel zurücksetzen
+
+"""
+
+def on_received_number(receivedNumber):
+    global hand_gegner
+    if receivedNumber == 99:
+        zurücksetzen()
+    else:
+        hand_gegner = receivedNumber
+radio.on_received_number(on_received_number)
+
+def zurücksetzen():
+    global mein_eigen, hand_gegner, gesendet
+    mein_eigen = 0
+    hand_gegner = -1
+    gesendet = 0
+
+def on_button_pressed_a():
+    global gesendet
+    if gesendet == 0:
+        radio.send_number(mein_eigen)
+        gesendet = 1
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def anzeigeErgebnis():
+    if mein_eigen == hand_gegner:
+        basic.show_icon(IconNames.ASLEEP)
+    elif mein_eigen == 1 and hand_gegner == 3:
+        basic.show_icon(IconNames.HAPPY)
+    elif mein_eigen == 2 and hand_gegner == 1:
+        basic.show_icon(IconNames.HAPPY)
+    elif mein_eigen == 3 and hand_gegner == 2:
+        basic.show_icon(IconNames.HAPPY)
+    else:
+        basic.show_icon(IconNames.SAD)
 
 def on_gesture_shake():
-    global hand
-    hand = randint(1, 3)
-    if hand == 1:
+    global mein_eigen, gesendet
+    mein_eigen = randint(1, 3)
+    gesendet = 0
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+def zeigeSSP():
+    if mein_eigen == 1:
         basic.show_leds("""
             # # . . #
             # # . # .
@@ -11,7 +67,7 @@ def on_gesture_shake():
             # # . # .
             # # . . #
             """)
-    elif hand == 2:
+    elif mein_eigen == 2:
         basic.show_leds("""
             . . . . .
             . # # # .
@@ -19,7 +75,7 @@ def on_gesture_shake():
             . # # # .
             . . . . .
             """)
-    elif hand == 3:
+    elif mein_eigen == 3:
         basic.show_leds("""
             # # # # #
             # . . . #
@@ -28,5 +84,28 @@ def on_gesture_shake():
             # # # # #
             """)
     else:
-        pass
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+        basic.show_leds("""
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            """)
+
+def on_button_pressed_ab():
+    zurücksetzen()
+    radio.send_number(99)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+gesendet = 0
+mein_eigen = 0
+hand_gegner = 0
+radio.set_group(1)
+zurücksetzen()
+
+def on_forever():
+    if gesendet == 1 and hand_gegner != -1:
+        anzeigeErgebnis()
+    else:
+        zeigeSSP()
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,52 +1,50 @@
 /**
- * Schütteln um ein zufälliges Bild auszusuchen
- */
-/**
  * Knopf A um das Bild zu senden
  */
 /**
  * Bild empfangen
  */
-/**
- * Spiel zurücksetzen
- */
+// Spiel zurücksetzen
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 99) {
         zurücksetzen()
     } else {
-        hand_funk = receivedNumber
+        hand_gegner = receivedNumber
     }
 })
 function zurücksetzen () {
-    hand = 0
-    hand_funk = -1
+    mein_eigen = 0
+    hand_gegner = -1
     gesendet = 0
 }
 input.onButtonPressed(Button.A, function () {
     if (gesendet == 0) {
-        radio.sendNumber(hand)
+        radio.sendNumber(mein_eigen)
         gesendet = 1
     }
 })
 function anzeigeErgebnis () {
-    if (hand == hand_funk) {
+    if (mein_eigen == hand_gegner) {
         basic.showIcon(IconNames.Asleep)
-    } else if (hand == 1 && hand_funk == 3) {
+    } else if (mein_eigen == 1 && hand_gegner == 3) {
         basic.showIcon(IconNames.Happy)
-    } else if (hand == 2 && hand_funk == 1) {
+    } else if (mein_eigen == 2 && hand_gegner == 1) {
         basic.showIcon(IconNames.Happy)
-    } else if (hand == 3 && hand_funk == 2) {
+    } else if (mein_eigen == 3 && hand_gegner == 2) {
         basic.showIcon(IconNames.Happy)
     } else {
         basic.showIcon(IconNames.Sad)
     }
 }
+/**
+ * Schütteln um ein zufälliges Bild auszusuchen
+ */
 input.onGesture(Gesture.Shake, function () {
-    hand = randint(1, 3)
+    mein_eigen = randint(1, 3)
     gesendet = 0
 })
 function zeigeSSP () {
-    if (hand == 1) {
+    if (mein_eigen == 1) {
         basic.showLeds(`
             # # . . #
             # # . # .
@@ -54,7 +52,7 @@ function zeigeSSP () {
             # # . # .
             # # . . #
             `)
-    } else if (hand == 2) {
+    } else if (mein_eigen == 2) {
         basic.showLeds(`
             . . . . .
             . # # # .
@@ -62,7 +60,7 @@ function zeigeSSP () {
             . # # # .
             . . . . .
             `)
-    } else if (hand == 3) {
+    } else if (mein_eigen == 3) {
         basic.showLeds(`
             # # # # #
             # . . . #
@@ -85,12 +83,12 @@ input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(99)
 })
 let gesendet = 0
-let hand = 0
-let hand_funk = 0
+let mein_eigen = 0
+let hand_gegner = 0
 radio.setGroup(1)
 zurücksetzen()
 basic.forever(function () {
-    if (gesendet == 1 && hand_funk != -1) {
+    if (gesendet == 1 && hand_gegner != -1) {
         anzeigeErgebnis()
     } else {
         zeigeSSP()
